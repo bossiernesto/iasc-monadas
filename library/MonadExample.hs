5aef22760fbb197bb087cb7478367b21f510beab
Ernesto Bossi <bossi.ernestog@gmail.com>
2020-06-08 15:29:33 -0300

    construirValidado naive

--- library/MonadExample.hs
+++ library/MonadExample.hs
@@ -1,12 +1,15 @@
-module MonadExample where
+-- la linea de abajo es para usar el typeclass Num
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module MonadExample(construirPersonajeValidado, validarNombre, Personaje(..), Validado(..)) where
 
 -- Agregamos un validado para validar el exito o el error de una validacion
-data Validado a = Exito a | Error String deriving Show
+data Validado a = Exito a | Error String deriving (Show, Eq)
 
 
 -- Con newtype definimos un nuevo tipo, el deriving lo dejamos mas adelante
 newtype Oro = Oro Int
-  deriving (Show)
+  deriving (Show, Eq, Num)
 
 -- Tenemos tambien los type alias...
 type Dinero = Oro
@@ -16,7 +19,7 @@ data Personaje = Personaje {
   nombre :: String,
   dinero :: Dinero,
   salud :: Int
-} deriving Show
+} deriving (Show, Eq)
 
 -- Ahora tenemos una funcion que nos va a validar que el nombre no sea demasiado corto 
 validarNombre :: String -> Validado String
@@ -27,4 +30,7 @@ validarNombre unNombre | length unNombre < 4 = Error "El nombre es muy corto"
 
 -- La idea seria de poder crear un personaje validado...                      
 construirPersonajeValidado :: Validado String -> Dinero -> Validado Personaje
-construirPersonajeValidado nombreValidado = undefined                       
+construirPersonajeValidado nombreValidado dinero = case nombreValidado of
+    Exito unNombre -> Exito (Personaje unNombre dinero 100)
+    Error mensajeDeError -> Error mensajeDeError
+                      
--- test-suite/Main.hs
+++ test-suite/Main.hs
@@ -5,6 +5,7 @@ import qualified Test.Tasty
 -- Hspec is one of the providers for Tasty. It provides a nice syntax for
 -- writing tests. Its website has more info: <https://hspec.github.io>.
 import Test.Tasty.Hspec
+import MonadExample
 
 main :: IO ()
 main = do
@@ -13,5 +14,14 @@ main = do
 
 spec :: Spec
 spec = parallel $ do
-    it "is trivially true" $ do
-        True `shouldBe` True
+    it "Deberia fallar cuando el nombre es muy corto..." $ do
+        let nombrePersonaje = validarNombre "inv"
+        (construirPersonajeValidado nombrePersonaje 100) == Error "El nombre es muy corto"
+
+    it "Deberia fallar cuando el nombre es demasiado largo..." $ do
+        let nombrePersonaje = validarNombre "esteNoEsUnNOmbreValidoAlSerDemasiadoLargo"
+        (construirPersonajeValidado nombrePersonaje 100) == Error "El nombre es muy largo"      
+
+    it "Deberia crearme el personaje cuando el nombre tiene la longitud correcta" $ do
+        let nombrePersonaje = validarNombre "NicoKnight"
+        (construirPersonajeValidado nombrePersonaje 250) == Exito (Personaje "NicoKnight" 250 100)         
